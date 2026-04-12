b3f01be72c55418f59754e84e8a344719b3a741f
ekuromiema <elizabethkuromiema@gmail.com>
2026-04-12 15:23:50 -0500

    working on freq table

--- HuffmanSource/HuffmanSource/SimpleHuffProcessor.java
+++ HuffmanSource/HuffmanSource/SimpleHuffProcessor.java
@@ -10,9 +10,9 @@
  *  UTEID: pg26747
  *  email address: pg26747
  *
- *  Student 2:
- *  UTEID:
- *  email address:
+ *  Student 2: Elizabeth Kuromiema
+ *  UTEID: eik254
+ *  email address: eik254@my.utexas.edu
  *
  *  Grader name:
  *  Section number:
@@ -27,14 +27,20 @@ public class SimpleHuffProcessor implements IHuffProcessor {
     private IHuffViewer myViewer;
 
     /**
+     * Frequency of each 8-bit value in the last preprocessed input (indices 0 .. ALPH_SIZE - 1).
+     */
+    private int[] myCounts;
+
+    /**
      * Preprocess data so that compression is possible ---
      * count characters/create tree/store state so that
      * a subsequent call to compress will work. The InputStream
      * is <em>not</em> a BitInputStream, so wrap it int one as needed.
-     * @param in is the stream which could be subsequently compressed
+     *
+     * @param in           is the stream which could be subsequently compressed
      * @param headerFormat a constant from IHuffProcessor that determines what kind of
-     * header to use, standard count format, standard tree format, or
-     * possibly some format added in the future.
+     *                     header to use, standard count format, standard tree format, or
+     *                     possibly some format added in the future.
      * @return number of bits saved by compression or some other measure
      * Note, to determine the number of
      * bits saved, the number of bits written includes
@@ -44,30 +50,39 @@ public class SimpleHuffProcessor implements IHuffProcessor {
      * @throws IOException if an error occurs while reading from the input file.
      */
     public int preprocessCompress(InputStream in, int headerFormat) throws IOException {
-        showString("Not working yet");
-        myViewer.update("Still not working");
-        throw new IOException("preprocess not implemented");
-        BitInputStream read = new BitInputStream(in);
-        //return 0;
-    }
-
-    private int[] getFrequencies() {
-
+        BitInputStream bitIn = new BitInputStream(in);
+        int[] counts = getFrequencies(bitIn);
+        bitIn.close();
+        return 0;
     }
 
     /**
-	 * Compresses input to output, where the same InputStream has
+     * Reads the stream one byte (8 bits) at a time and returns occurrence counts for each value.
+     */
+    private int[] getFrequencies(BitInputStream bitIn) throws IOException {
+        int[] counts = new int[IHuffConstants.ALPH_SIZE];
+        int bit = bitIn.readBits(BITS_PER_WORD);
+        while (bit != -1) {
+            counts[bit]++;
+            bit = bitIn.readBits(BITS_PER_WORD);
+        }
+        return counts;
+    }
+
+    /**
+     * Compresses input to output, where the same InputStream has
      * previously been pre-processed via <code>preprocessCompress</code>
      * storing state used by this call.
      * <br> pre: <code>preprocessCompress</code> must be called before this method
-     * @param in is the stream being compressed (NOT a BitInputStream)
-     * @param out is bound to a file/stream to which bits are written
-     * for the compressed file (not a BitOutputStream)
+     *
+     * @param in    is the stream being compressed (NOT a BitInputStream)
+     * @param out   is bound to a file/stream to which bits are written
+     *              for the compressed file (not a BitOutputStream)
      * @param force if this is true create the output file even if it is larger than the input file.
-     * If this is false do not create the output file if it is larger than the input file.
+     *              If this is false do not create the output file if it is larger than the input file.
      * @return the number of bits written.
      * @throws IOException if an error occurs while reading from the input file or
-     * writing to the output file.
+     *                     writing to the output file.
      */
     public int compress(InputStream in, OutputStream out, boolean force) throws IOException {
         throw new IOException("compress is not implemented");
@@ -77,22 +92,23 @@ public class SimpleHuffProcessor implements IHuffProcessor {
     /**
      * Uncompress a previously compressed stream in, writing the
      * uncompressed bits/data to out.
-     * @param in is the previously compressed data (not a BitInputStream)
+     *
+     * @param in  is the previously compressed data (not a BitInputStream)
      * @param out is the uncompressed file/stream
      * @return the number of bits written to the uncompressed file/stream
      * @throws IOException if an error occurs while reading from the input file or
-     * writing to the output file.
+     *                     writing to the output file.
      */
     public int uncompress(InputStream in, OutputStream out) throws IOException {
-	        throw new IOException("uncompress not implemented");
-	        //return 0;
+        throw new IOException("uncompress not implemented");
+        //return 0;
     }
 
     public void setViewer(IHuffViewer viewer) {
         myViewer = viewer;
     }
 
-    private void showString(String s){
+    private void showString(String s) {
         if (myViewer != null) {
             myViewer.update(s);
         }
